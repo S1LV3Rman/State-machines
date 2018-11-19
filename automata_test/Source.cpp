893a89b3fba5527fd2ef595d34107d45d43611a7
S1LV3Rman <38568293+S1LV3Rman@users.noreply.github.com>
2018-11-19 23:27:26 +0300

    Rebuild

--- automata_test/Source.cpp
+++ automata_test/Source.cpp
@@ -1,43 +1,27 @@
 #include "Automata.h"
 #include <iostream>
-#include <vector>
 
 using std::cout;
 using std::endl;
-using std::vector;
 
 int main()
 {
-	State s0("0");
-	State s1("1");
-	State s2("2");
-	State s3("3");
+	vector<int> states = { 0, 1, 2, 3 };
 
-	s0.addTransition('a', &s1);
-	s0.addTransition('b', &s1);
+	vector<vector<int>> d = { {1, 1}, {1, 2}, {2, 3}, {3, 0} };
 
-	s1.addTransition('a', &s1);
-	s1.addTransition('b', &s2);
-
-	s2.addTransition('a', &s2);
-	s2.addTransition('b', &s3);
-
-	s3.addTransition('a', &s3);
-	s3.addTransition('b', &s0);
-
-	Automata a;
+	Automata<int> a(states, "ab", d);
 	
 	string word = "abbababbba";
 
 	cout << "Word: " << word << endl;
 
-	vector<State*> States { &s0, &s1, &s2, &s3 };
+	set<int> s = { 0, 1, 2, 3 };
 
-	for (auto i : States)
-	{
-		a.setStart(i);
-		cout << "Starting is " << i->getLabel() << ", ending is - " << (a.inputWord(word)->getLabel()) << endl;
-	}
+	s = a.d(s, word);
+
+	for (auto i : s)
+		cout << i << ' ';
 
 	system("pause");
 
--- automata_test/automata.h
+++ automata_test/automata.h
@@ -1,19 +1,77 @@
 #pragma once
-#include"State.h"
+#include <vector>
+#include <set>
+#include <string>
+#include <map>
 
+
+using std::string;
+using std::set;
+using std::vector;
+using std::map;
+
+template <typename State>
 class Automata
 {
 private:
-	State* startState; // Начальное состояние 
-	State* currentState; // Текущее состояние
+	vector<State> _states;
+	string _letters;
+	map<State, map<char, State>> _transitions;
+
 public:
-	Automata();
-	Automata(State*);
+	Automata() {}
 
-	~Automata();
+	Automata(set<State> states, set<char> letters, vector<vector<State>> transitions)
+	{
+		for (auto i : states)
+			_states.push_back(i);
 
-	State* inputWord(string); // Ввод слова в автомат
-	void reset(); // Сброс автомата в начальное состояние
-	void setStart(State*); // Установка начального состояния
+		for (auto i : letters)
+			_letters.push_back(i);
+
+		for (int i = 0, n = transitions.size(); i < n; ++i)
+			for (int j = 0, m = transitions[i].size(); j < m; ++j)
+				_transitions[_states[i]][_letters[j]] = transitions[i][j];
+	}
+
+	Automata(vector<State> states, string letters, vector<vector<State>> transitions)
+	{
+		_states = states;
+		_letters = letters;
+
+		for (int i = 0, n = transitions.size(); i < n; ++i)
+			for (int j = 0, m = transitions[i].size(); j < m; ++j)
+				_transitions[_states[i]][_letters[j]] = transitions[i][j];
+	}
+
+	Automata(vector<State> states, string letters, map<State, map<char, State>> transitions)
+	{
+		_states = states;
+		_letters = letters;
+		_transitions = transitions;
+	}
+
+
+	~Automata()
+	{
+		_states.clear();
+		_letters.clear();
+		_transitions.clear();
+	}
+
+	set<State> d(set<State> states, string word)
+	{
+		for (auto letter : word)
+		{
+			set<State> nextStates;
+
+			for (auto state : states)
+				nextStates.insert(_transitions[state][letter]);
+
+			states = nextStates;
+		}
+
+		return states;
+	}
 };
 
